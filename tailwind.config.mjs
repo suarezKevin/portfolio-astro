851db130a80a5d84cd551180d62979e613174002
suarezKevin <suarezk453@gmail.com>
2024-01-11 23:05:39 -0500

    Load portfolio

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
